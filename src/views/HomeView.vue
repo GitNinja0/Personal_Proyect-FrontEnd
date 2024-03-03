ea2b963bc79def40c737e40335740c3c0b072622
Juan Munguia <carlosmunguia23145@gmail.com>
2024-03-03 22:56:02 +0100

    configured proyect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>hola</h1>
   </main>
 </template>
+<style scoped lang="scss">
+
+</style>
